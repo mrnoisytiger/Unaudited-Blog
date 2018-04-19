e606ac7a1e670f8446c421b9f2aa6b104b78e5b4
Felix Jen <fjen@felixjen.com>
2018-04-19 14:00:36 -0700

    Optimize images too

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,13 +52,28 @@ module.exports = function(grunt) {
                 }],
             },
         },
+
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 6
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'public/images',
+                    src: ['uploads/*.{png,jpg}', '*.{png,jpg}',],
+                    dest: 'public/images'
+                }],
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-concat-css');
     grunt.loadNpmTasks('grunt-processhtml');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    grunt.registerTask('default',['cssmin','concat_css','processhtml','htmlmin']);
+    grunt.registerTask('default',['cssmin','concat_css','processhtml','htmlmin','imagemin']);
     
 };
