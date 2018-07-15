9d34c1f58bffbc292cb368ea09a08dc2e7d43f2f
Felix Jen <fjen@felixjen.com>
2018-07-15 13:46:43 -0700

    Added more graphs

--- content/post/cuisine-and-demographics.md
+++ content/post/cuisine-and-demographics.md
@@ -8,6 +8,7 @@ tags:
 author: Felix Jen
 require:
   - chartjs
+  - chartjs-datalabels
   - latex
   - jquery
   - fontawesome
@@ -16,7 +17,7 @@ draft: false
 type: post
 ---
 
-Often times, we go to a town we've never been to and notice, "There are a lot of Asian restaurants here!" Does that have a tendency to correlate to there actually being a lot of Asian people in the area? The same can be asked about any other cuisine and its associated race. Upon initial thought, one would agree that there should indeed be a pretty close correlation. Otherwise, who else would be eating there, cooking there, etc...? It's pretty clear that ethnicity would prefer their own cuisine as opposed to others. Therefore one would expect the spread of restaurants across race to somewhat mimic that of the racial profile of the population in the area. This post seeks to examine the possible link between these two, or lack thereof.
+Often times, we go to a town we've never been to and notice, "There are a lot of Asian restaurants here!" Does that have a tendency to correlate to there actually being a lot of Asian people in the area? The same can be asked about any other cuisine and its associated race. Upon initial thought, one would agree that there should indeed be a pretty close correlation. Otherwise, who else would be eating there, cooking there, etc...? It’s pretty clear that people of a certain ethnicity would prefer their own cuisine as opposed to others. Therefore one would expect the spread of restaurants across race to somewhat mimic that of the racial profile of the population in the area. This post seeks to examine the possible link between these two, or lack thereof.
 
 ## Methodology
 
@@ -39,17 +40,17 @@ Where do we find data on restaurants within an area? [Yelp](http://www.yelp.com)
 ```php
 {
   search(term:"restaurant",
-  	location:"<zip code>",
+    location:"<zip code>",
     limit: 50,
     offset: 0,
   ){
     total
     business {
-    	name
+      name
       categories {
         title
       }
- 	  }
+    }
   }
 }
 ```
@@ -58,7 +59,37 @@ This query generates a list of 50 restaurants, displaying the restaurant's name 
 
 First off, Yelp's API only allows for a listing of 50 restaurants from this endpoint at a time and in total, only provides the first 1,000 restaurants in the particular zip code. Often, cities would have more than 1,000 restaurants, and most definitely more than 50. Therefore, for each ZIP code, the query was looped as many times as necessary to get all the restaurant data, each time changing the `offset` value to be 50 higher. As well, if a ZIP Code had more than 1,000 restaurants (which was the case for many), the data would be truncated to the first 1,000.
 
-Next, while Yelp does have category data for each of its restaurants listed, this data did not match exactly with racial markers. For example, a category for a restaurant might be `Korean`, whereas it actually corresponds to the race of `Asian`. A quick scan of Yelp's documentation revealed the [full list](https://www.yelp.com/developers/documentation/v2/category_list) of possible categories a restaurant could take. Then, a best effort attempt was made to allocate each Yelp category to a "race" designation. This was a bit tricky (and inevitably involves bias), because while these categories do mostly correspond to varying ethnicities, allocating them to fit "race" is not exactly a one-to-one task. For example, do we consider `Spanish` as "Hispanic/Latino" or "White"? What about `Filipino`, with influences from a variety of cultures? Such questions ultimately make the classification tricky and may inherently skew the data. As well, there were some Yelp categories which added little to no useful information, such as `salad` or `Food Court`. It's near impossible to say a specific race about a salad without knowing more, so categories like those were left out of consideration entirely. An allocated list of categories is available for download [here](ADD LINK TO DOWNLOAD). 
+Next, while Yelp does have category data for each of its restaurants listed, this data did not match exactly with racial markers. For example, a category for a restaurant might be `Korean`, whereas it actually corresponds to the race of `Asian`. A quick scan of Yelp's documentation revealed the [full list](https://www.yelp.com/developers/documentation/v2/category_list) of possible categories a restaurant could take. Then, a best effort attempt was made to allocate each Yelp category to a "race" designation. This was a bit tricky (and inevitably involves bias), because while these categories do mostly correspond to varying ethnicities, allocating them to fit "race" is not exactly a one-to-one task. For example, do we consider `Spanish` as "Hispanic/Latino" or "White"? What about `Filipino`, with influences from a variety of cultures? Such questions ultimately make the classification tricky and may inherently skew the data. As well, there were some Yelp categories which added little to no useful information, such as `salad` or `Food Court`. It's near impossible to say a specific race about a salad without knowing more, so categories like those were left out of consideration entirely. An allocated list of categories is available for download [here](ADD LINK TO DOWNLOAD). The number of categories allocated to each race is summarized in the chart below.
+
+<div class="canvas-div"><canvas id="cats-to-dem"></canvas></div>
+
+<!--
+<table>
+<tr>
+  <th>Race</th>
+  <th>Category Count</th>
+</tr>
+<tr>
+  <td>White</td>
+  <td>60</td>
+</tr>
+<tr>
+  <td>Asian</td>
+  <td>33</td>
+</tr>
+<tr>
+  <td>Black</td>
+  <td>6</td>
+</tr>
+<tr>
+  <td>Pac. Isl.</td>
+  <td>7</td>
+</tr>
+<tr>
+  <td>Hispanic</td>
+  <td>21</td>
+</tr>
+</table><br> -->
 
 A similar issue arises if a restaurant crosses multiple categories and happens to traverse multiple races. What if a restaurant serves both Asian and Mexican food or is some kind of modern fusion between the two? In those cases, those restaurants were considered to exist in *both* categories. Therefore, some of the data in each zip code may appear to show more than 1,000 restaurants total, but this is merely an artifact of the classification done. One may ask why such a choice was made, to consider a restaurant in more than one race. To answer that, we look back at our existing question, whether cuisine correlates to race. If a restaurant crosses into more than one racial type, an assumption was made (may be presumptuous) that each racial type represented would be equally likely to eat at the restaurant, and that the restaurant panders to each of those races.
 
@@ -68,7 +99,7 @@ With those considerations resolves, the Yelp API was scraped for the 50 zip code
 
 ### Per-ZIP-Code Comparison
 
-The following graphs represent a Per-Zip-Code comparison of the percent of restaurants per race, and the percent of calculated population per race.
+The following graphs represent a Per-Zip-Code comparison of the percent of restaurants per race, and the percent of calculated population per race. 
 
 <div class="lr-select">
   <i class="fas fa-arrow-left" id="left-arrow-zip-code"></i>
@@ -77,9 +108,20 @@ The following graphs represent a Per-Zip-Code comparison of the percent of resta
 </div>
 <div class="canvas-div-xl"><canvas id="per-zip-comparison"></canvas></div>
 
-Upon initial examine, we notice two major things. The majority of the restaurant data tends to look pretty similar, while the population data tends to vary wildly. As well, we notice that there are some places with shocking correlation, e.g. 90024, where there are also a great deal that completely miss the mark.
+Upon initial examination, we notice two major things. The majority of the restaurant data tends to look pretty similar, while the population data tends to vary wildly. As well, we notice that there are some places with shocking correlation, e.g. 90024, where there are also a great deal that completely miss the mark. This is extremely interesting because it suggests there are indeed some disconnects between an area's cuisine profile and its racial profile. We'll examine these more in depth by race in the later section, seeing if there is a specific discongruity somewhere. 
 
-<!-- Examine this data closer -->
+Additionally, flipping through all the presented zip codes in succession, we notice that the percentages of restaurants form a roughly similar shape, while the population demographics vary wildly. This can be easily explained by the way we classified the restaurants discussed earlier. Looking through our classification list, we can see that `White` had the most categories assigned to it, followed by Asian, then Hispanic. This was simply due to the race of `White` being a very large, all-encompassing race, from American, to European, to Middle-Eastern. This is in contrast to races like `Black`, which is much more stringent in classification. A deeper dive into this issue will be performed in a later section.
+
+### Per-Race Data Scatterplots
+
+After seeing the per-zip-code data, it makes sense to dive into the predictive value of cuisine profile to racial profile. To do this, we'll look at each race separately in its own scatterplot and attempt to perform regression analysis on the data.
+
+<div class="lr-select">
+  <i class="fas fa-arrow-left" id="left-arrow-race"></i>
+  <p id="current-race"></p>
+  <i class="fas fa-arrow-right" id="right-arrow-race"></i>
+</div>
+<div class="canvas-div-xl"><canvas id="per-race-comparison"></canvas></div>
 
 **Note: Not yet complete**
 
@@ -112,5 +154,29 @@ Upon initial examine, we notice two major things. The majority of the restaurant
 
 .lr-select i:first-child {
   color: lightgrey;
+}
+
+table {
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    font-size: 1.1rem;
+    width: 100%;
+}
+
+th {
+    width: 25%;
+    background-color: rgb(4,125,161);
+    color: white;
+    height: 35px;
+    border-radius: 5px;
+}
+
+td {
+    height: 30px;
+}
+
+tr:nth-child(even) {
+    background-color: rgba(4,125,161, 0.3);
 }
 </style>
--- static/js/post/cuisine-and-demographics.js
+++ static/js/post/cuisine-and-demographics.js
@@ -1,7 +1,24 @@
 var yelp_data;
 var pop_data;
 var zips_list = [];
-var chart_one;
+var race_list = ['white','asian','black','api','hispanic'];
+var race_map_list = {
+    'white': 'White',
+    'asian': 'Asian',
+    'black': 'Black',
+    'api': 'Pac. Isl.',
+    'hispanic': 'Hispanic'
+};
+var chart_one; // Category to Race Data
+var chart_two; // Per Zip Data
+var chart_three;
+
+// search for the datalabels plugin
+var datalabels = Chart.plugins.getAll().filter(function(p) {
+    return p.id === 'datalabels';
+})[0];
+// globally unregister the plugin
+Chart.plugins.unregister(datalabels);
 
 // Pull the data files and call to initialize graphs
 $(document).ready(function() {
@@ -13,19 +30,79 @@ $(document).ready(function() {
             for ( var single_zip in pop_data ) {
                 zips_list.push(single_zip);
             }
+
+            Chart.defaults.global.responsive = true;
+            Chart.defaults.global.maintainAspectRatio = false;
+
+            initialize_cat_to_dem_data();
             initialize_per_zip_data(zips_list[0]);
+            initialize_per_race_data(race_list[0]);
         });
     });
 
+    // Initialize Event Handlers for button clicks
     $("#right-arrow-zip-code").click(function() {
-        per_buttons_change('right')
+        per_zip_buttons_change('right')
     });
     $("#left-arrow-zip-code").click(function() {
-        per_buttons_change('left')
+        per_zip_buttons_change('left')
+    });
+    $("#right-arrow-race").click(function() {
+        per_race_buttons_change('right')
+    });
+    $("#left-arrow-race").click(function() {
+        per_race_buttons_change('left')
     });
 });
 
 // Initialize the graphs. Here's where the actual code begins.
+function initialize_cat_to_dem_data() {
+    var ctx_one = document.getElementById('cats-to-dem').getContext('2d');
+    chart_one = new Chart(ctx_one, {
+        type: 'doughnut',
+        data: {
+            labels: ['White', 'Asian', 'Black', 'Pac. Isl.', 'Hispanic'],
+            datasets: [{
+                data: [60,33,6,7,21],
+                backgroundColor: [
+                    'rgba(7,180,231, 0.6)',
+                    'rgba(231,7,180,0.6)',
+                    'rgba(180,231,7,0.6)',
+                    'rgba(94,12,232,0.6)',
+                    'rgba(232,147,12,0.6)',
+                ],
+                datalabels: {
+                    color: 'black',
+                    font: {
+                      weight: 'bold',
+                      size: '16',
+                    },
+                    formatter: Math.round,
+                }
+            }],
+        },
+        options:{
+            title: {
+                display: true,
+                text: "Categories Per Race",
+                fontFamily: "'Lato','Helvetica Neue',Helvetica,sans-serif",
+                fontStyle: "bold",
+                fontSize: "20",
+                fontColor: "#000",
+                padding: 10,
+            },
+            tooltips: {
+                enabled: false
+            }
+        },
+        plugins: [
+            datalabels
+        ],
+        responsive: true,
+        maintainAspectRatio: false
+    })
+}
+
 function initialize_per_zip_data(zip) {
     $('#current-zip-code').attr('data-current-zip', zip);
     $('#current-zip-code').html(zip);
@@ -33,12 +110,9 @@ function initialize_per_zip_data(zip) {
     var per_zip_pop_data = [];
     get_per_zip_data(zip,per_zip_yelp_data,per_zip_pop_data);
 
-    Chart.defaults.global.responsive = true;
-    Chart.defaults.global.maintainAspectRatio = false;
-
     // Per-ZIP Data
-    var ctx_one = document.getElementById('per-zip-comparison').getContext('2d');
-    chart_one = new Chart(ctx_one, {
+    var ctx_two = document.getElementById('per-zip-comparison').getContext('2d');
+    chart_two = new Chart(ctx_two, {
         type: 'radar',
         data: {
             labels: ['White','Asian','Black','Pac. Isl.','Hispanic'],
@@ -81,6 +155,56 @@ function initialize_per_zip_data(zip) {
     });
 }
 
+function initialize_per_race_data(race) {
+    $('#current-race').attr('data-current-race', race);
+    $('#current-race').html(race_map_list[race]);
+    var per_race_data = [];
+    get_per_race_data(race,per_race_data);
+
+    var ctx_three = document.getElementById('per-race-comparison').getContext('2d');
+    chart_three = new Chart(ctx_three, {
+        type: 'scatter',
+        data: {
+            datasets: [{
+                label: "Race",
+                data: per_race_data,
+                backgroundColor: 'rgba(7,180,231, 0.6)',
+                pointRadius: 5,
+            }],
+        },
+        options: {
+            title: {
+                display: true,
+                text: "Per Race Data",
+                fontFamily: "'Lato','Helvetica Neue',Helvetica,sans-serif",
+                fontStyle: "bold",
+                fontSize: "20",
+                fontColor: "#000",
+                padding: 10,
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Population %',
+                        fontSize: 14
+                    }
+                }],
+                xAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Restaurant %',
+                        fontSize: 14
+                    }
+                }],
+            }
+        }
+    })
+}
+
 function change_per_zip_data(zip, chart) {
     $('#current-zip-code').attr('data-current-zip', zip);
     $('#current-zip-code').html(zip);
@@ -93,6 +217,17 @@ function change_per_zip_data(zip, chart) {
     chart.update();
 }
 
+function change_per_race_data(race, chart) {
+    $('#current-race').attr('data-current-race', race);
+    $('#current-race').html(race_map_list[race]);
+    
+    var data_var = [];
+    get_per_race_data(race,data_var);
+
+    chart.data.datasets[0].data = data_var;
+    chart.update();
+}
+
 function get_per_zip_data(zip, yelp_var, pop_var) {
     var zip_yelp_total = yelp_data[zip]['restaurants']['total'];
     var zip_pop_total = pop_data[zip]['population']['total'];
@@ -100,43 +235,84 @@ function get_per_zip_data(zip, yelp_var, pop_var) {
     for ( race in yelp_data[zip]['restaurants'] ) {
         if ( race !== 'total' ) {
         var race_yelp_count = yelp_data[zip]['restaurants'][race];
-        var race_yelp_percent = race_yelp_count/zip_yelp_total * 100;
-        race_yelp_percent = +race_yelp_percent.toFixed(2);
-        yelp_var.push(race_yelp_percent);
+        yelp_var.push(toPercent(race_yelp_count,zip_yelp_total));
         }
     }
 
     for ( race in pop_data[zip]['population'] ) {
         if ( race !== 'total' ) {
             var race_pop_count = pop_data[zip]['population'][race];
-            var race_pop_percent = race_pop_count/zip_pop_total * 100;
-            race_pop_percent = +race_pop_percent.toFixed(2);
-            pop_var.push(race_pop_percent);
+            pop_var.push(toPercent(race_pop_count,zip_pop_total));
         }
     }
 }
 
-function per_buttons_change(direction) {
+function get_per_race_data(race, data_var) {
+    for (var zip in yelp_data ) {
+        var race_yelp_percent = toPercent( yelp_data[zip]['restaurants'][race],yelp_data[zip]['restaurants']['total'] );
+        var race_pop_percent = toPercent( pop_data[zip]['population'][race],pop_data[zip]['population']['total'] );
+        
+        var data_point = {
+            'x': race_yelp_percent,
+            'y': race_pop_percent,
+        }
+        
+        data_var.push(data_point);
+    }
+}
+
+function toPercent(part,total) {
+    var perc = part/total * 100;
+    perc = +perc.toFixed(2);
+    return perc;
+}
+
+function per_zip_buttons_change(direction) {
     current_zip = $('#current-zip-code').attr('data-current-zip');
     current_zip_index = zips_list.indexOf(current_zip);
 
     if ( direction == "left" ) {
         if ( current_zip_index <= 1 ) {
-            change_per_zip_data(zips_list[0], chart_one);
+            change_per_zip_data(zips_list[0], chart_two);
             $("#left-arrow-zip-code").css("color", "lightgrey");
         } else {
-            change_per_zip_data(zips_list[current_zip_index-1], chart_one);
+            change_per_zip_data(zips_list[current_zip_index-1], chart_two);
             $("#left-arrow-zip-code").css("color", "black");
             $("#right-arrow-zip-code").css("color", "black");
         }
     } else {
         if ( current_zip_index >= zips_list.length - 2) {
-            change_per_zip_data(zips_list[zips_list.length - 1], chart_one);
+            change_per_zip_data(zips_list[zips_list.length - 1], chart_two);
             $("#right-arrow-zip-code").css("color", "lightgrey");
         } else {
-            change_per_zip_data(zips_list[current_zip_index + 1], chart_one);
+            change_per_zip_data(zips_list[current_zip_index + 1], chart_two);
             $("#left-arrow-zip-code").css("color", "black");
             $("#right-arrow-zip-code").css("color", "black");
+        }
+    }
+}
+
+function per_race_buttons_change(direction) {
+    current_race = $('#current-race').attr('data-current-race');
+    current_race_index = race_list.indexOf(current_race);
+
+    if ( direction == "left" ) {
+        if ( current_race_index <= 1 ) {
+            change_per_race_data(race_list[0], chart_three);
+            $("#left-arrow-race").css("color", "lightgrey");
+        } else {
+            change_per_race_data(race_list[current_race_index-1], chart_three);
+            $("#left-arrow-race").css("color", "black");
+            $("#right-arrow-race").css("color", "black");
+        }
+    } else {
+        if ( current_race_index >= race_list.length - 2) {
+            change_per_race_data(race_list[race_list.length - 1], chart_three);
+            $("#right-arrow-race").css("color", "lightgrey");
+        } else {
+            change_per_race_data(race_list[current_race_index + 1], chart_three);
+            $("#left-arrow-race").css("color", "black");
+            $("#right-arrow-race").css("color", "black");
         }
     }
 }
